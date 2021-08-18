33d195c72a909d02faf7fb60fa2b924f8a0c71e0
samuditha <samudithahasantha12@gmail.com>
2021-08-18 20:34:17 +0530

    created family UI s

---
+++ app/src/main/java/com/example/red_bubble/closeChildMode.java
@@ -0,0 +1,14 @@
+package com.example.red_bubble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class closeChildMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_close_child_mode);
+    }
+}

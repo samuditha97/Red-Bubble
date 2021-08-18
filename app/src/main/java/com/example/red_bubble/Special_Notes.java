9e8d9c72a93016cc3861b6a966c7cb5a6225adfd
LihiniPerera <lihinilekani@gmail.com>
2021-08-18 22:37:43 +0530

    Add official UI files

---
+++ app/src/main/java/com/example/red_bubble/Special_Notes.java
@@ -0,0 +1,14 @@
+package com.example.red_bubble;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Special_Notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_special_notes);
+    }
+}
